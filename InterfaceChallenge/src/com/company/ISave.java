3b42279433b2b89933beda9886dd3f4f9207e30e
Duncan <duncan.macleod@salesforce.com>
2017-05-08 17:22:13 -0700

    Interface Challenge and begin Inner Classes

---
+++ InterfaceChallenge/src/com/company/ISave.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by duncan.macleod on 5/8/17.
+ */
+public interface ISave {
+
+    List<String> write();
+    void read(List<String> savedValues);
+}
